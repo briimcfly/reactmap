0bad28f5673a03b09d9a1bb33cac11847248612b
Bryan Bickel <briimcfly@gmail.com>
2025-10-22 11:17:54 -0700

    ready to deploy

--- floor-demo/src/AssetTracker.jsx
+++ floor-demo/src/AssetTracker.jsx
@@ -3,49 +3,45 @@ import './AssetTracker.css';
 
 const AssetTracker = () => {
     const [peopleLocations, setPeopleLocations] = useState([]);
-
-    // Define your floor plan areas with grid positioning
-    // gridColumn: "start / span X" where X is number of columns to span
-    // gridRow: "start / span X" where X is number of rows to span
     const areas = [
         // Row 1
         {
-            id: 'dz-den',
+            id: '495283',
             name: 'DZ Den',
             type: 'Conference Room',
             gridColumn: '1 / span 1',
             gridRow: '1 / span 1'
         },
         {
-            id: 'stealth',
+            id: '385847',
             name: 'Stealth',
             type: 'Conference Room',
             gridColumn: '2 / span 1',
             gridRow: '1 / span 1'
         },
         {
-            id: 'dz-zen-den',
+            id: '495291',
             name: 'The Cove',
             type: 'Cubicles/Seating',
             gridColumn: '3 / span 1',
             gridRow: '1 / span 1'
         },
         {
-            id: 'supply-office',
+            id: '495285',
             name: 'Supply Office',
             type: 'Office',
             gridColumn: '4 / span 1',
             gridRow: '1 / span 1'
         },
         {
-            id: 'cto-office',
+            id: '495286',
             name: 'CTO Office',
             type: 'Office',
             gridColumn: '5 / span 1',
             gridRow: '1 / span 1'
         },
         {
-            id: 'president-office',
+            id: '495287',
             name: 'President Office',
             type: 'Office',
             gridColumn: '6 / span 1',
@@ -54,28 +50,28 @@ const AssetTracker = () => {
 
         // Row 2
         {
-            id: 'back-room',
+            id: '495322',
             name: 'Back Room',
             type: 'Cubicles/Seating',
             gridColumn: '1 / span 2',
             gridRow: '2 / span 2'
         },
         {
-            id: 'front-room',
+            id: '495321',
             name: 'Front Room',
             type: 'Cubicles/Seating',
             gridColumn: '3 / span 2',
             gridRow: '2 / span 2'
         },
         {
-            id: '4insite-lobby',
+            id: '495323',
             name: '4insite Lobby',
             type: 'Entry/Lobby',
             gridColumn: '6 / span 1',
             gridRow: '2 / span 1'
         },
         {
-            id: 'discovery',
+            id: '385846',
             name: 'Discovery',
             type: 'Conference Room',
             gridColumn: '5 / span 1',
@@ -83,7 +79,7 @@ const AssetTracker = () => {
         },
         // Row 3
         {
-            id: '4insite-kitchen',
+            id: '495290',
             name: '4insite Kitchen',
             type: 'Kitchen',
             gridColumn: '5 / span 2',
@@ -91,14 +87,14 @@ const AssetTracker = () => {
         },
         //Row 4 
         {
-            id: 'womens',
+            id: '495289',
             name: 'Womens',
             type: 'Restroom',
             gridColumn: '2 / span 1',
             gridRow: '4 / span 1'
         },
         {
-            id: 'mens',
+            id: '495288',
             name: 'Mens',
             type: 'Restroom',
             gridColumn: '3 / span 1',
@@ -110,7 +106,7 @@ const AssetTracker = () => {
     // Fetch people locations from your API
     const fetchPeopleLocations = async () => {
         try {
-            const response = await fetch('YOUR_API_ENDPOINT_HERE');
+            const response = await fetch('https://bay4.4insite.com/api/geofence/employee/current-area/?contract_id=259&employee_ids=131218%2C6711');
             const data = await response.json();
             
             // Transform the API response to match our component's expected format
@@ -121,7 +117,7 @@ const AssetTracker = () => {
                     id: checkIn.employee.id,
                     name: `${checkIn.employee.first_name} ${checkIn.employee.last_name}`,
                     avatar: checkIn.employee.first_name.charAt(0).toUpperCase(),
-                    areaId: checkIn.current_area.id.toString(), // Use the area ID directly
+                    areaId: checkIn.current_area.id.toString(),
                     photoUrl: checkIn.employee.photo_url || null
                 };
             });
@@ -146,7 +142,7 @@ const AssetTracker = () => {
 
     return (
         <div className="asset-tracker">
-            <h1>4insite BLE Demo</h1>
+            <h1>4invasion of Privacy</h1>
             <div className="floor-plan">
                 {areas.map(area => {
                     const peopleInArea = getPeopleInArea(area.id);
@@ -166,11 +162,11 @@ const AssetTracker = () => {
                             <div className="people-container">
                                 {peopleInArea.map(person => (
                                     <div key={person.id} className="person-avatar" title={person.name}>
-                                        {person.photoUrl ? (
+                                        {/* {person.photoUrl ? (
                                             <img src={person.photoUrl} alt={person.name} className="avatar-image" />
-                                        ) : (
-                                            person.avatar
-                                        )}
+                                        ) : ( */}
+                                            {person.avatar}
+                                        {/* )} */}
                                     </div>
                                 ))}
                             </div>
